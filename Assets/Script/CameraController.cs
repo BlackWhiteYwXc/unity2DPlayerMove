b13d863278eae082f75cb65b6d30905a3fa3abb4
BlackWhiteYwXc <1678550510@qq.com>
2024-11-28 17:39:53 +0800

    0.0.2（未完善）
    目前有移动，跳跃功能。
    优化了跳跃宽容度
    优化了跳跃下落时重力变化
    新增摄像机跟随、摄像机视角限制

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform PlayerTransform;
+    private Vector3 velocity = Vector3.zero;//移动速度
+    public Vector3 positionOffset = new Vector3(-5,0,0);//摄像机偏移距离
+    [Range(0,1)]
+    public float smoothTime = 0.1f;
+    [Header("摄像机限制范围(最小值，最大值)")]
+    public Vector2 xLimit;//X方向限制范围
+    public Vector2 yLimit;//Y方向限制范围
+
+    private void Awake()
+    {
+        PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private Vector3 targetPosition;
+    private void LateUpdate()
+    {
+        targetPosition = PlayerTransform.position + positionOffset;
+        targetPosition = new Vector3(Mathf.Clamp(targetPosition.x,xLimit.x,xLimit.y),Mathf.Clamp(targetPosition.y,yLimit.x,yLimit.y),-10);
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
